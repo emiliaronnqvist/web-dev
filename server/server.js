675e561d7eda8ad133268fdd9c775d5282bfe549
AntonKinnander <a.kinnander@icloud.com>
2026-02-19 12:13:59 +0100

    Store slugs in data

--- server/server.js
+++ server/server.js
@@ -13,6 +13,13 @@ app.get("/stores", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/assets/stores.json"));
 });
 
+//To show specific store pages Borde vi ändra och lägga in slug utan svenska tecken så urln blir /ahlens istället för /åhlens?
+app.get("/stores/:id", (req, res) => {
+  const id = req.params.id;
+  const store = stores.find((store) => store.id === id);
+  res.json(store);
+});
+
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
