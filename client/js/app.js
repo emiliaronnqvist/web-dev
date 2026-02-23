9bd8e750cae9bbb646546b806d1e40b0f977b622
LovisaSkoogh <sklo23zz@student.ju.se>
2026-02-23 11:12:47 +0100

    no changes

--- client/js/app.js
+++ client/js/app.js
@@ -1,11 +1,11 @@
 // Wait until the page is loaded
 document.addEventListener("DOMContentLoaded", () => {
   fetch("/stores")
-    .then(res => res.json())
-    .then(data => {
+    .then((res) => res.json())
+    .then((data) => {
       const list = document.getElementById("stores");
 
-      data.forEach(store => {
+      data.forEach((store) => {
         const li = document.createElement("li");
 
         li.innerHTML = `
@@ -18,5 +18,5 @@ document.addEventListener("DOMContentLoaded", () => {
         list.appendChild(li);
       });
     })
-    .catch(err => console.error("Error loading stores:", err));
+    .catch((err) => console.error("Error loading stores:", err));
 });
