ef8fbc74ed0bce5d04c454a3e530fc4f64a52ad2
AntonKinnander <a.kinnander@icloud.com>
2026-02-23 11:26:19 +0100

    Dom content loaded solves every issue as always

--- client/js/app.js
+++ client/js/app.js
@@ -1,42 +1,42 @@
 // Wait until the HTML page is loaded
-// document.addEventListener("DOMContentLoaded", () => {
-//Fetches all stores from the server for index.html
-const list = document.getElementById("stores");
-if (list) {
-  fetch("/stores")
-    .then((res) => res.json())
-    .then((data) => {
-      data.forEach((store) => {
-        const li = document.createElement("li");
+document.addEventListener("DOMContentLoaded", () => {
+  //Fetches all stores from the server for index.html
+  const list = document.getElementById("stores");
+  if (list) {
+    fetch("/stores")
+      .then((res) => res.json())
+      .then((data) => {
+        data.forEach((store) => {
+          const li = document.createElement("li");
 
-        li.innerHTML = `
+          li.innerHTML = `
             <h2>${store.name}</h2>
             <a href="/store/${store.slug}">Visit</a>
             <hr>
           `;
 
-        list.appendChild(li);
-      });
-    })
-    .catch((err) => console.error("Error", err));
-}
+          list.appendChild(li);
+        });
+      })
+      .catch((err) => console.error("Error", err));
+  }
 
-//Fetches a specific store from the server for store.html templat
-const div = document.getElementById("store_info");
-if (div) {
-  // Extract slug from the URL path
-  const pathParts = window.location.pathname.split("/");
-  const slug = pathParts[pathParts.length - 1];
+  //Fetches a specific store from the server for store.html templat
+  const div = document.getElementById("store_info");
+  if (div) {
+    // Extract slug from the URL path
+    const pathParts = window.location.pathname.split("/");
+    const slug = pathParts[pathParts.length - 1];
 
-  fetch(`/stores/${slug}`)
-    .then((res) => res.json())
-    .then((store) => {
-      div.innerHTML = `
+    fetch(`/stores/${slug}`)
+      .then((res) => res.json())
+      .then((store) => {
+        div.innerHTML = `
           <h2>${store.name}</h2>
           <p>${store.description}</p>
           <a href="http://${store.url}" target="_blank">Visit ${store.name}</a>
         `;
-    })
-    .catch((err) => console.error("Error loading store:", err));
-}
-// });
+      })
+      .catch((err) => console.error("Error loading store:", err));
+  }
+});
