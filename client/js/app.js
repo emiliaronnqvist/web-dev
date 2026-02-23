2c94e643f3a271e9959f9e8bf7353a43b87a0e5f
AntonKinnander <a.kinnander@icloud.com>
2026-02-23 11:02:51 +0100

    Store not wotrking yet

--- client/js/app.js
+++ client/js/app.js
@@ -1,22 +1,38 @@
 // Wait until the page is loaded
 document.addEventListener("DOMContentLoaded", () => {
+  //Fetches all stores from the server for index.html
   fetch("/stores")
-    .then(res => res.json())
-    .then(data => {
+    .then((res) => res.json())
+    .then((data) => {
       const list = document.getElementById("stores");
 
-      data.forEach(store => {
+      data.forEach((store) => {
         const li = document.createElement("li");
 
         li.innerHTML = `
           <h2>${store.name}</h2>
           <p>${store.description}</p>
-          <a href="${store.url}" target="_blank">Visit</a>
+          <a href="/store/${store.slug}" target="_blank">Visit</a>
           <hr>
         `;
 
         list.appendChild(li);
       });
     })
-    .catch(err => console.error("Error loading stores:", err));
+    .catch((err) => console.error("Error loading stores:", err));
+
+  //Fetches a specific store from the server for store.html template
+  fetch(`/stores/${slug}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const div = document.getElementById("store_info");
+
+      div.innerHTML = `
+        <h2>${store.name}</h2>
+        <p>${store.description}</p>
+        <a href="${store.url}" target="_blank">Visit ${store.name}</a>
+        <hr>
+      `;
+    })
+    .catch((err) => console.error("Error loading store:", err));
 });
